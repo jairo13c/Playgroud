4edc91e6431d6784515344d3aa7874e0b1d5933e
jairo13c <31705891+jairo13c@users.noreply.github.com>
2017-10-04 13:24:38 -0600

    i did a red button.

---
+++ GUI Playground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "working with GUI :D"
+
+
+private var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width:600, height: 120))
+
+
+
+
+myFirstButton.setTitle("words", for: .normal)
+
+
+
+
+
+myFirstButton.backgroundColor = .red
+
+
+
